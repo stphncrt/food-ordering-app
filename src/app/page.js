7d33322934d8ef0213db626aed5562b21d607ca5
KMSGROEP\akdal.huseyin <akdal.huseyin@kpmg.nl>
2024-01-15 23:34:43 +0100

    register page created

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
 import SectionHeaders from "@/components/layout/SectionHeaders";
@@ -6,7 +5,6 @@ import SectionHeaders from "@/components/layout/SectionHeaders";
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <HomeMenu />
       <section className="text-center my-16">
@@ -44,9 +42,6 @@ export default function Home() {
           </a>
         </div>
       </section>
-      <footer className="border-t p-8 text-center text-gray-500 mt-16">
-        &copy; 2023 All rights reserved
-      </footer>
     </>
   );
 }
---
+++ src/app/register/page.js
@@ -0,0 +1,5 @@
+function RegisterPage() {
+  return <div>Enter</div>;
+}
+
+export default RegisterPage;
