2d500ce6cd4c6817b55591d5f14210e260c3453b
KMSGROEP\akdal.huseyin <akdal.huseyin@kpmg.nl>
2024-01-28 23:44:30 +0100

    Database connected

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link className="text-primary font-semibold text-4xl" href="/">
+        <Link className="text-primary font-semibold text-4xl" href={"/"}>
           ST PIZZA
         </Link>
         <Link href={"/"}>Home</Link>
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Right from "@/components/icons/Right";
+import Right from "../icons/Right";
 export default function Hero() {
   return (
     <section className="hero">
